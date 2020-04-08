1d82f9b70ecec74251675b8a892bd51d0732424a
leofranca47 <leoprofissional@live.com>
2020-04-08 10:03:43 -0300

    efeito de rolagem no menu

--- js/script.js
+++ js/script.js
@@ -3,4 +3,9 @@ $(function(){
         $('.main_header_content_menu_mobile_sub').toggleClass('ds_none');
         $(this).toggleClass('main_header_content_menu_mobile_obj_active');
     });
+
+    $('ul > li > a.animate').on('click', function(event){
+            event.preventDefault();
+            $('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);  
+    });
 });
